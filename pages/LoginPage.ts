f16f3cbdf4c95a26aa096794fe704a9352ded732
= <rangarajan.ragavan@gmail.com>
2026-07-05 12:33:06 +0530

    csv test data implementation

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -2,12 +2,14 @@ import { Browser, Locator, Page } from "@playwright/test";
 import { ElementUtil } from "../util/ElementUtil";
 import { title } from "node:process";
 import { HomePage } from "./HomePage";
+import { RegisterPage } from "./RegisterPage";
 export class LoginPage {
   private readonly page: Page;
   private readonly elu: ElementUtil;
   private readonly emailId: Locator;
   private readonly password: Locator;
   private readonly loginButton: Locator;
+  private readonly registerLink: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -15,6 +17,7 @@ export class LoginPage {
     this.emailId = page.getByRole("textbox", { name: "E-Mail Address" });
     this.password = page.getByRole("textbox", { name: "Password" });
     this.loginButton = page.locator(`input[value='Login']`);
+    this.registerLink = page.getByRole("link", { name: "Register" });
   }
 
   async navigatetoLogin(baseURL: string | undefined) {
@@ -27,5 +30,9 @@ export class LoginPage {
     await this.elu.click(this.loginButton);
     let titlePage = await this.page.title();
     return new HomePage(this.page);
+  }
+  async gotoRegistration(): Promise<RegisterPage> {
+    await this.elu.click(this.registerLink);
+    return new RegisterPage(this.page);
   }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,41 @@
 import { expect, test } from "../fixtures/baseFixture";
+import { LoginPage } from "../pages/LoginPage";
+import { parse } from "csv-parse/sync";
+import fs from "fs";
 
+type Dataschema = {
+  firstName: string;
+  lastName: string;
+  telephone: string;
+  password: string;
+  subscribeNewsletter: string;
+};
+let fileContent = fs.readFileSync("./testdata/register.csv", "utf-8");
+let registrationData: Dataschema[] = parse(fileContent, {
+  columns: true,
+  skip_empty_lines: true,
+});
 test("Login to portal", async ({ homePage }) => {
   await expect(homePage.page).toHaveTitle("My Account");
 });
+
+for (let user of registrationData) {
+  test(`Register Email ${user.firstName} `, async ({ page, baseURL }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigatetoLogin(baseURL);
+    const register = await loginPage.gotoRegistration();
+
+    await register.registerUser(
+      user.firstName,
+      user.lastName,
+      getRandomEmail(),
+      user.telephone,
+      user.password,
+      user.subscribeNewsletter,
+    );
+  });
+}
+
+function getRandomEmail(): string {
+  return `auto${Date.now()}@gmail.com`;
+}
