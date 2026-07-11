b1136eb07105527a1b505fa45b9f59de290db348
= <rangarajan.ragavan@gmail.com>
2026-07-11 14:28:40 +0530

    changing base fixture and adding env file

--- fixtures/baseFixture.ts
+++ fixtures/baseFixture.ts
@@ -9,13 +9,17 @@ type MyFixture = {
 export const test = base.extend<MyFixture>({
   homePage: async ({ page, baseURL }, use, testInfo) => {
     const loginPage = new LoginPage(page);
-    loginPage.navigatetoLogin(baseURL);
+
+    // Wait for navigation to complete
+    await loginPage.navigatetoLogin(baseURL);
+
     const userName = testInfo.project.metadata.appUserName;
     const password = testInfo.project.metadata.appPassWord;
 
-    let homePage = await loginPage.logintohomePage(userName, password);
-    //await page.waitForTimeout(4000);
-    expect(homePage.isUserLoggedIn()).toBeTruthy();
+    const homePage = await loginPage.logintohomePage(userName, password);
+
+    // Wait for the async login verification
+    expect(await homePage.isUserLoggedIn()).toBeTruthy();
 
     await use(homePage);
   },
